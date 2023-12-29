980aac3ec3234f177f3e58d9ab124d2b3d904a09
Sergey <pristup2040@gmail.com>
2023-12-29 13:36:56 +1000

    some update

--- script.js
+++ script.js
@@ -8,11 +8,20 @@ const GameStep = { FIRST_CHOSE: 1, SECOND_CHOSE: 2, GAME_RESULT: 3 };
 class Game {
     constructor() {
         this.init_game()
+        this.init_html()
     }
 
     init_html(){
-
+        for (const element of document.querySelectorAll(".door")) {
+            element.setAttribute("data-game_door", "no-choose")
+        }
+        document.querySelector(".stats__total-games").textContent = localStorage.getItem("total_games") - 0
+        document.querySelector(".stats__change_door-win").textContent = localStorage.getItem("change_win") - 0
+        document.querySelector(".stats__change_door-lose").textContent = localStorage.getItem("change_lose") - 0
+        document.querySelector(".stats__not_change_door-win").textContent = localStorage.getItem("not_change_win") - 0
+        document.querySelector(".stats__not_change_door-lose").textContent = localStorage.getItem("not_change_lose") - 0
     }
+
     init_game(){
         this.game_step = GameStep.FIRST_CHOSE;
         this.win_door = randomIntFromInterval(1, 3)
@@ -27,8 +36,7 @@ class Game {
 
 let game = new Game()
 
-let doors = document.querySelectorAll(".door")
-for (const element of doors) {
+for (const element of document.querySelectorAll(".door")) {
     element.addEventListener('click', function () {
         let door_status = element.getAttribute("data-game_door")
         console.log(door_status, element)
