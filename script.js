69d4a7199742eed45a0e5e797eb0eddf1278fa41
Sergey <pristup2040@gmail.com>
2023-12-29 15:16:24 +1000

    work step 2

--- script.js
+++ script.js
@@ -11,16 +11,22 @@ class Game {
         this.init_html()
     }
 
-    init_html(){
+    init_doors_html(){
         for (const element of document.querySelectorAll(".door")) {
             element.setAttribute("data-game_door", "no-choose")
         }
+        document.querySelector(".info_text").textContent = "Выберите дверь!"
+    }
+    init_stats_html(){
         document.querySelector(".stats__total-games").textContent = localStorage.getItem("total_games") - 0
         document.querySelector(".stats__change_door-win").textContent = localStorage.getItem("change_win") - 0
         document.querySelector(".stats__change_door-lose").textContent = localStorage.getItem("change_lose") - 0
         document.querySelector(".stats__not_change_door-win").textContent = localStorage.getItem("not_change_win") - 0
         document.querySelector(".stats__not_change_door-lose").textContent = localStorage.getItem("not_change_lose") - 0
-        document.querySelector(".info_text").textContent = "Выберите дверь!"
+    }
+    init_html(){
+        this.init_stats_html()
+        this.init_doors_html()
     }
 
     init_game(){
@@ -50,10 +56,49 @@ class Game {
             }
             document.querySelectorAll(".door")[open_door_number-1].setAttribute("data-game_door", "open-lose")
             document.querySelector(".info_text").textContent = "Изменить выбор?"
-
-
             this.game_step = GameStep.SECOND_CHOSE
+            return
         }
+
+        if (this.game_step === GameStep.SECOND_CHOSE) {
+            if (door.getAttribute("data-game_door") === "open-lose") return;
+
+            for (const element of document.querySelectorAll(".door")) {
+                if (element.getAttribute('data-door-number')-0 === this.win_door) {
+                    element.setAttribute("data-game_door", "open-win")
+                } else {
+                    element.setAttribute("data-game_door", "open-lose")
+                }
+            }
+
+            let final_door_number = door.getAttribute("data-door-number") - 0
+            // console.log("Первый выбор:", this.first_door_number)
+            // console.log("Второй выбор:", final_door_number)
+            // console.log("Результат:", door.getAttribute("data-door-number")-0 === this.win_door)
+
+            if (door.getAttribute("data-door-number")-0 === this.win_door) {
+                document.querySelector(".info_text").textContent = "Вы выиграли"
+                if (this.first_door_number === final_door_number) {
+                    localStorage.setItem("not_change_win", localStorage.getItem("not_change_win")-0+1)
+                } else {
+                    localStorage.setItem("change_win", localStorage.getItem("change_win")-0+1)
+                }
+            } else {
+                document.querySelector(".info_text").textContent = "Вы проиграли"
+                if (this.first_door_number === final_door_number) {
+                    localStorage.setItem("not_change_lose", localStorage.getItem("not_change_lose")-0+1)
+                } else {
+                    localStorage.setItem("change_lose", localStorage.getItem("change_lose")-0+1)
+                }
+            }
+
+            localStorage.setItem("total_games", localStorage.getItem("total_games")-0+1)
+            this.game_step = GameStep.GAME_RESULT
+            this.init_stats_html()
+
+        }
+
+
         // door.addEventListener()
     }
 }
@@ -64,7 +109,7 @@ for (const element of document.querySelectorAll(".door")) {
     element.addEventListener('click', function () {
         game.door_event(this)
         let door_status = element.getAttribute("data-game_door")
-        console.log(door_status, element)
+        // console.log(door_status, element)
 
     })
 }
